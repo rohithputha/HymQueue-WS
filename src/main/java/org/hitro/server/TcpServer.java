4876dcf739b3c84f5d9d04baed384164c5d4275f
Rohith <rohith@192.168.1.5>
2023-08-07 01:04:25 +0530

    added support to idle connection manager

--- src/main/java/org/hitro/server/TcpServer.java
+++ src/main/java/org/hitro/server/TcpServer.java
@@ -4,6 +4,7 @@ import org.hitro.conn.ConnectionHandler;
 import org.hitro.publicinterfaces.HymQueue;
 import org.hitro.exceptions.HymQueueException;
 import org.hitro.services.ConnectionExecutors;
+import org.hitro.services.IdleConnectionsManager;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -12,18 +13,27 @@ import java.net.Socket;
 public class TcpServer {
     private final ServerSocket serverSocket;
     private final HymQueue hymQueue;
-    private TcpServer(ServerSocket serverSocket, HymQueue hymQueue)  {
+
+    private final IdleConnectionsManager idleConnectionsManager;
+    private TcpServer(ServerSocket serverSocket, HymQueue hymQueue, IdleConnectionsManager idleConnectionsManager)  {
         this.serverSocket = serverSocket;
         this.hymQueue = hymQueue;
+        this.idleConnectionsManager = idleConnectionsManager;
     }
 
     public void listen(){
         System.out.println("server listening");
+
         while(true){
             try{
                 Socket socket =  this.serverSocket.accept();
                 System.out.println("connect request received");
-                ConnectionExecutors.getInstance().addConnection(new ConnectionHandler(socket, this.hymQueue));
+                ConnectionHandler connectionHandler = new ConnectionHandler(socket,this.hymQueue);
+                Thread thread = new Thread(connectionHandler);
+                idleConnectionsManager.addCh(connectionHandler,thread);
+                System.out.println("Active Connection Number = "+idleConnectionsManager.getChSize());
+                thread.start();
+//                ConnectionExecutors.getInstance().addConnection(new ConnectionHandler(socket, this.hymQueue));
             }
             catch (IOException ioe){
                 throw new HymQueueException("Failed to accept socket connection",ioe);
@@ -40,7 +50,10 @@ public class TcpServer {
             synchronized (TcpServer.class){
                 if(tcpServer == null){
                     try {
-                        tcpServer = new TcpServer(new ServerSocket(3456), new HymQueue());
+                        IdleConnectionsManager idleConnectionsManager1 = new IdleConnectionsManager();
+                        Thread idleConnectionManagerThread = new Thread(idleConnectionsManager1);
+                        idleConnectionManagerThread.start();
+                        tcpServer = new TcpServer(new ServerSocket(3456), new HymQueue(), idleConnectionsManager1);
                     } catch (IOException e) {
                         throw new HymQueueException("Failed to acquire port for the HymQueue-WS",e);
                     }
